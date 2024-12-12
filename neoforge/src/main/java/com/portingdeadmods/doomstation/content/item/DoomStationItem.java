217a40b3d55f66b1a0ec9788e8d1487de0dcda0c
ReclipseTheOne <narcisprecup233@gmail.com>
2024-12-12 17:54:39 +0200

    Screen

--- neoforge/src/main/java/com/portingdeadmods/doomstation/content/item/DoomStationItem.java
+++ neoforge/src/main/java/com/portingdeadmods/doomstation/content/item/DoomStationItem.java
@@ -1,7 +1,15 @@
 package com.portingdeadmods.doomstation.content.item;
 
+import com.portingdeadmods.doomstation.content.screen.DoomStationScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class DoomStationItem extends Item {
     public DoomStationItem(Properties properties) {
@@ -11,5 +19,16 @@ public class DoomStationItem extends Item {
     @Override
     public int getMaxStackSize(ItemStack stack) {
         return 1;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        ItemStack itemstack = player.getItemInHand(usedHand);
+        if (!level.isClientSide) {
+            return InteractionResultHolder.pass(itemstack);
+        }
+
+        Minecraft.getInstance().setScreen(new DoomStationScreen(Component.literal("DoomStation 1")));
+        return InteractionResultHolder.pass(player.getItemInHand(usedHand));
     }
 }
---
+++ neoforge/src/main/java/com/portingdeadmods/doomstation/content/screen/DoomStationScreen.java
@@ -0,0 +1,12 @@
+package com.portingdeadmods.doomstation.content.screen;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class DoomStationScreen extends Screen {
+    private final String type;
+    public DoomStationScreen(Component title) {
+        super(title);
+        this.type = title.toString();
+    }
+}
